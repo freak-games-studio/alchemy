5dcb220e14aec1182085fb8fab5a589f64427136
Vitalij Ryndin <crashmax.dev@gmail.com>
2023-10-28 00:12:26 +0800

    fix sounds

--- apps/frontend/src/stores/use-sounds.ts
+++ apps/frontend/src/stores/use-sounds.ts
@@ -1,13 +1,13 @@
 import { defineStore } from 'pinia'
 
 export const useSounds = defineStore('sounds', () => {
-  const createAudio = new Audio('/sounds/create.mp3')
+  const createAudio = new Audio('sounds/create.mp3')
   createAudio.volume = 0.2
 
-  const createNewAudio = new Audio('/sounds/create-new.mp3')
+  const createNewAudio = new Audio('sounds/create-new.mp3')
   createNewAudio.volume = 0.3
 
-  const takingAudio = new Audio('/sounds/taking.mp3')
+  const takingAudio = new Audio('sounds/taking.mp3')
   takingAudio.volume = 0.5
 
   return {
